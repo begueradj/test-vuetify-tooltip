85e02f98212d42fd4bbd4c1ff32d3a6888130f28
begueradj <begueradj.bill@gmail.com>
2020-01-30 08:58:38 +0200

    Add component with tooltip

---
+++ components/ComponentWithTooltip.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on }">
+            <v-icon v-on="on" color="grey">
+              help
+            </v-icon>
+          </template>
+          <span>
+            {{ tooltipText }}
+          </span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ComponentWithTooltip',
+  props: {
+    tooltipText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
